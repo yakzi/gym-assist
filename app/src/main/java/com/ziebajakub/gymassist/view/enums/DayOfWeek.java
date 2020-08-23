6065a01ce585b78046db26e4ca7517c345bf6a3b
yakzi <zieba.jakub.97@gmail.com>
2020-08-23 22:38:16 +0200

    FIX add sunday to DayOfWeek & add getName method for 1st upper case

--- app/src/main/java/com/ziebajakub/gymassist/view/enums/DayOfWeek.java
+++ app/src/main/java/com/ziebajakub/gymassist/view/enums/DayOfWeek.java
@@ -1,5 +1,9 @@
 package com.ziebajakub.gymassist.view.enums;
 
 public enum DayOfWeek {
-     MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
+     MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY;
+
+     public String getName() {
+          return name().charAt(0) + name().substring(1).toLowerCase();
+     }
 }
