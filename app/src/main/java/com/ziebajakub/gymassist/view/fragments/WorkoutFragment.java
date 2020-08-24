56e883a36a78e1b6d50c7933ea9c08008032e84f
yakzi <zieba.jakub.97@gmail.com>
2020-08-24 19:55:04 +0200

    ADD setting current workout with prev & next buttons

--- app/src/main/java/com/ziebajakub/gymassist/view/fragments/WorkoutFragment.java
+++ app/src/main/java/com/ziebajakub/gymassist/view/fragments/WorkoutFragment.java
@@ -10,9 +10,11 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.ziebajakub.gymassist.R;
 import com.ziebajakub.gymassist.databinding.FragmentWorkoutBinding;
 import com.ziebajakub.gymassist.services.models.User;
 import com.ziebajakub.gymassist.services.models.Workout;
+import com.ziebajakub.gymassist.view.enums.DayOfWeek;
 import com.ziebajakub.gymassist.view.interfaces.Constants;
 import com.ziebajakub.gymassist.viewmodels.WorkoutViewModel;
 
@@ -20,8 +22,9 @@ import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
 
-public class WorkoutFragment extends BaseFragment {
+public class WorkoutFragment extends BaseFragment implements View.OnClickListener {
 
     private FragmentWorkoutBinding binding;
     private User user;
@@ -58,15 +61,37 @@ public class WorkoutFragment extends BaseFragment {
 
         initViewModels();
         initView();
+        setListeners();
 
         return binding.getRoot();
     }
 
+    private void setListeners() {
+        binding.workoutPrevDayButton.setOnClickListener(this);
+        binding.workoutNextDayButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.workout_next_day_button:
+                setCurrentWorkout(currentWorkout.getDay().ordinal() + 1);
+                break;
+            case R.id.workout_prev_day_button:
+                setCurrentWorkout(currentWorkout.getDay().ordinal() - 1);
+        }
+    }
+
     @SuppressLint("SimpleDateFormat")
     private void initView() {
-        binding.workoutDayDate.setText(new SimpleDateFormat("dd.MM.YYYY")
-                .format(Calendar.getInstance().getTime()));
-        currentWorkout = workoutList.get(0);
+        Calendar now = Calendar.getInstance();
+        binding.workoutDayDate.setText(new SimpleDateFormat("dd.MM.YYYY").format(now.getTime()));
+        setCurrentWorkout(now.get(Calendar.DAY_OF_WEEK) - 2);
+    }
+
+    private void setCurrentWorkout(int day) {
+        int dayValue = day >= 7 ? 0 : day < 0 ? 6 : day;
+        currentWorkout = workoutList.get(dayValue);
         binding.workoutDayName.setText(currentWorkout.getDay().getName());
     }
 
