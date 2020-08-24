2cdc15e2284cce5c658dc013ac88868ffbe10544
yakzi <zieba.jakub.97@gmail.com>
2020-08-24 20:43:16 +0200

    ADD exercises list adapter & open add exercise dialog

--- app/src/main/java/com/ziebajakub/gymassist/view/fragments/WorkoutFragment.java
+++ app/src/main/java/com/ziebajakub/gymassist/view/fragments/WorkoutFragment.java
@@ -1,20 +1,26 @@
 package com.ziebajakub.gymassist.view.fragments;
 
 import android.annotation.SuppressLint;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.google.android.material.textfield.TextInputEditText;
 import com.ziebajakub.gymassist.R;
+import com.ziebajakub.gymassist.databinding.DialogAddExerciseBinding;
 import com.ziebajakub.gymassist.databinding.FragmentWorkoutBinding;
 import com.ziebajakub.gymassist.services.models.User;
 import com.ziebajakub.gymassist.services.models.Workout;
-import com.ziebajakub.gymassist.view.enums.DayOfWeek;
+import com.ziebajakub.gymassist.view.adapters.ExerciseAdapter;
 import com.ziebajakub.gymassist.view.interfaces.Constants;
 import com.ziebajakub.gymassist.viewmodels.WorkoutViewModel;
 
@@ -22,7 +28,8 @@ import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.List;
-import java.util.Locale;
+
+import static com.ziebajakub.gymassist.view.interfaces.Constants.ERROR_INPUT;
 
 public class WorkoutFragment extends BaseFragment implements View.OnClickListener {
 
@@ -31,6 +38,7 @@ public class WorkoutFragment extends BaseFragment implements View.OnClickListene
     private WorkoutViewModel workoutViewModel;
     private List<Workout> workoutList;
     private Workout currentWorkout;
+    private ExerciseAdapter adapter;
 
     public WorkoutFragment() {
     }
@@ -69,6 +77,7 @@ public class WorkoutFragment extends BaseFragment implements View.OnClickListene
     private void setListeners() {
         binding.workoutPrevDayButton.setOnClickListener(this);
         binding.workoutNextDayButton.setOnClickListener(this);
+        binding.workoutAddExerciseButton.setOnClickListener(this);
     }
 
     @Override
@@ -79,7 +88,39 @@ public class WorkoutFragment extends BaseFragment implements View.OnClickListene
                 break;
             case R.id.workout_prev_day_button:
                 setCurrentWorkout(currentWorkout.getDay().ordinal() - 1);
+                break;
+            case R.id.workout_add_exercise_button:
+                openAddExerciseDialog();
         }
+    }
+
+    private void openAddExerciseDialog() {
+        if (getContext() != null) {
+            Dialog dialog = new Dialog(getContext());
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            dialog.setCancelable(true);
+            DialogAddExerciseBinding binding = DialogAddExerciseBinding.inflate(LayoutInflater.from(getContext()));
+            dialog.setContentView(binding.getRoot());
+            binding.dialogAddExerciseButtonAdd.setOnClickListener(view -> {
+                if (validateInput(binding.dialogAddExerciseNameInput, false)
+                        && validateInput(binding.dialogAddExerciseRepsInput, true)
+                        && validateInput(binding.dialogAddExerciseSetsInput, true)
+                        && validateInput(binding.dialogAddExerciseWeightInput, true)) {
+                    //todo add exercise
+                    dialog.dismiss();
+                } else {
+                    Toast.makeText(getContext(), ERROR_INPUT, Toast.LENGTH_SHORT).show();
+                }
+            });
+            binding.dialogAddExerciseButtonCancel.setOnClickListener(view -> dialog.dismiss());
+            dialog.show();
+        }
+    }
+
+    private boolean validateInput(TextInputEditText input, boolean notZero) {
+        boolean textVal = input.getText() != null && !input.getText().toString().trim().isEmpty();
+        boolean numberVal = !notZero || input.getText() != null && Double.parseDouble(input.getText().toString()) > 0;
+        return textVal && numberVal;
     }
 
     @SuppressLint("SimpleDateFormat")
@@ -87,6 +128,9 @@ public class WorkoutFragment extends BaseFragment implements View.OnClickListene
         Calendar now = Calendar.getInstance();
         binding.workoutDayDate.setText(new SimpleDateFormat("dd.MM.YYYY").format(now.getTime()));
         setCurrentWorkout(now.get(Calendar.DAY_OF_WEEK) - 2);
+        binding.workoutDayExercisesList.setLayoutManager(new LinearLayoutManager(getContext()));
+        adapter = new ExerciseAdapter(getContext(), currentWorkout.getExercises());
+        binding.workoutDayExercisesList.setAdapter(adapter);
     }
 
     private void setCurrentWorkout(int day) {
