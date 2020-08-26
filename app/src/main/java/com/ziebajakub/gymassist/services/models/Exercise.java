98e01bd30a702f66ac0bf99d75cb023d6aaab601
yakzi <zieba.jakub.97@gmail.com>
2020-08-26 20:53:13 +0200

    FIX exercise constructor with adding one rep, set & weight element

--- app/src/main/java/com/ziebajakub/gymassist/services/models/Exercise.java
+++ app/src/main/java/com/ziebajakub/gymassist/services/models/Exercise.java
@@ -15,12 +15,16 @@ public class Exercise implements Serializable {
     public Exercise() {
     }
 
-    public Exercise(String id, String name, int rep, int set, int weight) {
+    public Exercise(String id, String name, History rep, History set, History weight) {
         this.id = id;
         this.name = name;
-        this.reps = new ArrayList<>(rep);
-        this.sets = new ArrayList<>(set);
-        this.weights = new ArrayList<>(weight);
+        this.reps = new ArrayList<>();
+        this.sets = new ArrayList<>();
+        this.weights = new ArrayList<>();
+
+        this.reps.add(rep);
+        this.sets.add(set);
+        this.weights.add(weight);
     }
 
     public String getId() {
