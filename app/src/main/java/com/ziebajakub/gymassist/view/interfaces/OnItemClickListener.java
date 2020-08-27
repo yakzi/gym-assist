f19f4fef43109060dc882007736cd1db5cb0feea
yakzi <zieba.jakub.97@gmail.com>
2020-08-27 19:00:57 +0200

    ADD OnItemClickListener interface

---
+++ app/src/main/java/com/ziebajakub/gymassist/view/interfaces/OnItemClickListener.java
@@ -0,0 +1,7 @@
+package com.ziebajakub.gymassist.view.interfaces;
+
+import android.view.View;
+
+public interface OnItemClickListener {
+    void onItemClick(Object object, int position, View view);
+}
